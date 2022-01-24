a8fc18bb72e5cbf1b64aa7d739dbb3eb88bac30f
gianlutech <gianluca.ingrao95@gmail.com>
2022-01-24 15:27:01 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,8 @@
+console.log('JS OK');
+/* 
+Generare 5 numeri casuali e mostrarli in un alert all'utente.
+Dall'ok  parte un timer di 30 secondi. (ricordate che l'alert blocca il flusso. il timer partirà a contare dopo che avete schiacciato ok.)
+Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri  sono stati indovinati dall'utente.
+*/
+
